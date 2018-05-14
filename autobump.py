7190000871444101ff73180133e1027ce33f0624
Takaki Oshima <takakioshima@gmail.com>
2018-05-14 18:59:39 +0100

    Add autobump script

---
+++ autobump.py
@@ -0,0 +1,34 @@
+
+import argparse
+import requests
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("email")
+    parser.add_argument("password")
+    args = parser.parse_args()
+
+    session = requests.Session()
+    resp = session.get("https://www.grailed.com")
+    token = (resp.text.split('<meta name="csrf-token" content="')[1]
+             .split('"')[0])
+    headers = {'x-csrf-token': token}
+
+    data = {'email': args.email, 'password': args.password}
+    resp = session.post("https://www.grailed.com/api/sign_in", data=data)
+    login = resp.json()['data']
+
+    url = ("https://www.grailed.com/api/users/{}/wardrobe"
+           .format(login['user']['id']))
+    resp = session.get(url)
+    items = resp.json()['data']
+    for item in items:
+        url = "https://www.grailed.com/api/listings/{}/bump".format(item['id'])
+        resp = session.post(url, headers=headers)
+        bumped = "" if 'error' in resp.json() else "bumped!"
+        print(u"{:<50} $ {:<6} \u2764 {:<6} {}".format(
+            item['designer']['name'] + " " + item['title'],
+            item['price'],
+            item['follower_count'],
+            bumped))
